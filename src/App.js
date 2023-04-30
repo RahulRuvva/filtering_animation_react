ec3ee4f74024c6f82010998f320554bf5bb24bc6
RahulRuvva <90190891+RahulRuvva@users.noreply.github.com>
2023-04-30 18:47:40 +0530

    Rendered Data from external API and used Grid to style the data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {React, useEffect, useState} from 'react'
+import Movie from './components/Movie';
+
 
 function App() {
+
+  const[popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async() => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=916d7540f0930e5b5672d21346277302&language=en-US&page=1');
+    const movies = await data.json();
+    console.log(movies);
+    setPopular(movies.results);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <h1>LORD JESUS help me out!! </h1>
+    <div className='popular-movies'>
+      {popular.map((movie) => {
+          return  <Movie key={movie.id} movie={movie}/>
+
+      })}
+    </div>
     </div>
   );
 }
